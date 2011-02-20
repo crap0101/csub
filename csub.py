0c9e02f78436708f4024fd5e5a0917277483a5ee
Marco Chieppa <crap0101@riseup.net>
2011-02-20 05:55:06 +0100

    add support for ass/ssa subtitle

--- csub.py
+++ csub.py
@@ -35,6 +35,8 @@ Examples:
 import os
 import re
 import sys
+import math
+import operator
 import itertools
 import warnings
 from tempfile import mkstemp
@@ -44,6 +46,23 @@ from optparse import OptionParser, OptionValueError
 #####################
 # F U N C T I O N S #
 #####################
+
+
+def numslice(n, i, keep_sign=False):
+    '''
+    Return the slice of the *i* most significant digs of *n*
+    for *i* > 0 or the less significant digits for *i* < 0 .
+    Preserve the sign if *keep_sign* is a true value (default: False).
+    '''
+    n, sign_op = (n, operator.pos) if n > 0 else (abs(n), operator.neg)
+    if not (i and n):
+        return sign_op(n) #raise ValueError('invalid number: %d' % i)
+    b = int(math.log10(n)) + 1
+    if abs(i) > b:
+        return sign_op(n) #raise ValueError('invalid number: %d' % i)
+    m, g = ((b-i), 0) if i > 0 else (abs(i), 1)
+    return sign_op(divmod(n, 10**m)[g]) if keep_sign else divmod(n, 10**m)[g]
+
 
 def check_ms (option, opt_str, value, parser, **kwords):
     if not (-999 <= value <= 999):
@@ -168,12 +187,13 @@ class Subtitle (object):
     """Base class implementing common function for time manipulation
     and subtitles formatting.
     """
-    def __init__ (self, str_format, re_pattern):
+    def __init__ (self, str_format='', re_pattern='.*'):
         self.STRING_FORMAT = str_format
         self.RE_MATCH_TIME = re.compile(re_pattern)
         self.RE_MATCH_NUMBER = re.compile('^-{0,1}\d+$')
         self.MAX_H = 3600
         self.MAX_MIN = 60
+        self.MAX_HNDRS = 100 # for ssa/ass
         self.MAX_MS = 1000
         self.delta_hour = self.delta_min = self.delta_sec = 0
         self.delta_ms = self.delta_sub_num = 0
@@ -191,8 +211,8 @@ class Subtitle (object):
             return number >= start and number < stop
         return is_edit
     
-    def set_delta (self, hour, min_, sec, ms, sub_number):
-        """Set time's attribute. """
+    def set_delta (self, hour=0, min_=0, sec=0, ms=0, sub_number=0):
+        """Set time's attribute."""
         self.delta_hour = hour
         self.delta_min = min_
         self.delta_sec = sec
@@ -261,12 +281,77 @@ class Subtitle (object):
         return hour, m / self.MAX_MIN, total_sec % self.MAX_MIN
 
 
+class AssSub (Subtitle):
+    """Class to manage *.ass/*.ssa subtitle. Inherit from Subtitle."""
+    def __init__ (self, file_in, file_out, unsafe_time_mode=False):
+        """`file_in' and `file_out' must be file-like objects."""
+        reg = '(^Dialogue: \d+),(\d{1}:\d{2}:\d{2}([:\.])\d{2}),(\d{1}:\d{2}:\d{2}([:.])\d{2}),(.*$)'
+        reg_unsafe = '(^Dialogue: \d+),(-{0,1}\d+:\d{2}:\d{2}([:\.])\d{2}),(-{0,1}\d+:\d{2}:\d{2}([:.])\d{2}),(.*$)'
+        time_reg = '(\d{1}):(\d{2}):(\d{2})[:\.](\d{2})'
+        time_reg_unsafe = '(-{0,1}\d+):(\d{2}):(\d{2})[:\.](\d{2})'
+        # Why these regex? well...
+        # The spec for ass/ssa file talk about a time format like
+        # 0:00:00:00  (so, *1* digit for hours and the colon as divider)
+        # ie. Hrs:Mins:Secs:hundredths
+        # BUT... some ass/ssa file use dot between Secs and hundredths,
+        # and many player accept this; so, we want to preserve the original sep.
+        self.reg = (re.compile(reg) if not unsafe_time_mode
+                    else re.compile(reg_unsafe))
+        self.time_reg = (re.compile(time_reg) if not unsafe_time_mode
+                                        else re.compile(time_reg_unsafe))
+        super(AssSub, self).__init__()
+        self.file_in = file_in
+        self.file_out = file_out
+        self.actual_numline = 0
+        self.MAX_MS = self.MAX_HNDRS
+        # NOTE: since ass/ssa use 2-digit precision for fraction of seconds
+        # *MAX_MS* is used as an alias for *MAX_HNDRS* and *self.delta_ms*
+        # used in various methods of the base class refer to hundreds, not
+        # milliseconds (to avoid rewrite methos in the base class).
+        
+    def new_time(self, time_string, sec_sep):
+        h, m, s, hndrs = map(int, self.time_reg.match(time_string).groups())
+        total_secs, hndrs = self.new_time_tuple(h, m, s, hndrs)
+        h, m, s = self.times_from_secs(total_secs)
+        return "%d:%02d:%02d%s%02d" % (h, m, s, sec_sep, hndrs) #numslice(hndrs, 2))
+
+    def parse_line (self, line):
+        """Fields are:
+          Marked, Start,End,Style,Name,MarginL,MarginR,MarginV,Effect,Text
+        we care about fields 1 and 2.
+        """
+        line = line.rstrip()
+        if line.startswith('Dialogue:'):
+            try:
+                match = self.reg.match(line)
+                init, start, start_sep, end, end_sep, rest = match.groups()
+                new_start = self.new_time(start, start_sep)
+                new_end = self.new_time(end, end_sep)
+                return ','.join((init, new_start, new_end, rest))
+            except ValueError, err:
+                raise MismatchTimeError("(%s) Something went wrong "
+                                        "computing this line: %s" % (err, line))
+            except AttributeError, err:
+                raise MismatchTimeError("You probably need to "
+                                        "--back-to-the-future\n"
+                                        "ERR LINE IS: %s" % line)            
+        return line
+
+    def set_delta(self, hour=0, min_=0, sec=0, hndrs=0, *not_used):
+        super(AssSub, self).set_delta(hour, min_, sec)
+        self.delta_ms = hndrs
+    
+    def main (self):
+        for self.actual_numline, line in itertools.izip(
+            itertools.count(1), self.file_in):
+            self.file_out.write("%s\n" % self.parse_line(line))
+
+        
 class SrtSub (Subtitle):
-    """Class to manage *.srt subtitle. Inherit from Subtitle.
-    `file_in' and `file_out' must be file-like objects.
-    """
+    """Class to manage *.srt subtitle. Inherit from Subtitle."""
 
     def __init__ (self, file_in, file_out, unsafe_time_mode=False, unsafe_number_mode=False):
+        """`file_in' and `file_out' must be file-like objects."""
         self.time_sep = " --> "
         self.string_format = "%02d:%02d:%02d,%03d"
         reg_safe = r'^(-{0,1}\d{2,}):(\d{2}):(\d{2}),(\d{3})$'
@@ -333,6 +418,9 @@ if __name__ == '__main__':
     parser = OptionParser(version="csub %s" % VERSION)
     parser.add_option("--info", dest="info", action="store_true",
                       help="print informations about the program and exit.")
+    parser.add_option("-t", "--type",type="string",
+                      dest="subtitle_type", metavar="TYPE",
+                      help="subtitle file type: ass, ssa, srt.")
     parser.add_option("-o", "--output-file",type="string",
                       dest="outfile", metavar="FILE",
                       help="write the subtitle in FILE (default: stdout).")
@@ -351,20 +439,24 @@ if __name__ == '__main__':
     parser.add_option("-m", "--milliseconds", type="int", action="callback",
                       callback_kwargs={'opt_dest':"ms",}, callback=check_ms,
                       metavar="NUMBER", dest="ms", default=0,
-                      help="change the milliseconds values by NUMBER."
+                      help="change the milliseconds values by NUMBER. "
                       "(NOTE: this value must be in range -999..999).")
     parser.add_option("-n", "--num", type="int", dest="num",
                       default=0, metavar="NUMBER",
-                      help="change the progressive subtitle number by NUMBER.")
+                      help="change the progressive subtitle number by NUMBER. "
+                      "(only for srt).")
     parser.add_option("-r", "--range", type="str",
                       dest="range", default=':', metavar="START:END",
-                      help="apply changes only for subs between START and END (excluded).")
-    parser.add_option("-b", "--back-to-the-future", action="store_true", dest="unsafe_time_mode",
-                      default=False, help="unsafe time mode. Don't get any errors if"
-                                          " timecode become negative.")
-    parser.add_option("-B", "--back-to-the-block", action="store_true", dest="unsafe_number_mode",
-                      default=False, help="unsafe number mode. Don't get any errors if"
-                                          " sub's numbers became negative.")
+                      help="apply changes only for subs between START"
+                      " and END (excluded). (only for srt).")
+    parser.add_option("-b", "--back-to-the-future", action="store_true",
+                      dest="unsafe_time_mode", default=False,
+                      help="unsafe time mode. Don't get any errors if "
+                      " timecode become negative.")
+    parser.add_option("-B", "--back-to-the-block", action="store_true",
+                      dest="unsafe_number_mode", default=False,
+                      help="unsafe number mode. Don't get any errors if "
+                      "sub's numbers became negative.  (only for srt).")
     parser.add_option("-w", "--warn", action="store_true", dest="is_warn",
                       default=False, help="enable warnings.")
 
@@ -383,17 +475,34 @@ if __name__ == '__main__':
             in_file = open(opts.infile, "r")
         if opts.outfile:
             out_file = open(opts.outfile, "w")
-    newsub = SrtSub(in_file, out_file, opts.unsafe_time_mode, opts.unsafe_number_mode)
+    # temporary ugly if/elif:
+    if not opts.subtitle_type:
+        parser.error("subtitle type (-t/--type) must be specified!")
+    if opts.subtitle_type in ('ass', 'ssa'):
+        newsub = AssSub(in_file, out_file, opts.unsafe_time_mode)
+    elif opts.subtitle_type == 'srt':
+        newsub = SrtSub(in_file, out_file,
+                        opts.unsafe_time_mode, opts.unsafe_number_mode)
+        start_sub, end_sub = opts.range.split(':')
+        newsub.set_subs_range(int(start_sub) if start_sub else None,
+                              int(end_sub) if end_sub else None)
     newsub.set_delta(opts.hour, opts.min, opts.sec, opts.ms, opts.num)
-    start_sub, end_sub = opts.range.split(':')
-    newsub.set_subs_range(int(start_sub) if start_sub else None,
-                          int(end_sub) if end_sub else None)
     newsub.IS_WARN = opts.is_warn
+    ass_opt_warn = 'WARNING: ignoring %s options with ass/ssa subtitles\n'
+    if opts.is_warn and opts.subtitle_type in ('ass', 'ssa'):
+        if opts.range != ':':
+            sys.stderr.write(ass_opt_warn % '-r/--range')
+        if opts.num:
+            sys.stderr.write(ass_opt_warn % '-n/--num')
+        #if opts.unsafe_time_mode:
+        #    sys.stderr.write(ass_opt_warn % '-b/--back-to-the-future')
+        if opts.unsafe_number_mode:
+            sys.stderr.write(ass_opt_warn % '-B/--back-to-the-block')
     try:
         newsub.main()
-    except (MismatchTimeError, IndexNumError), e:
-        sys.stderr.write("[at line %d] %s: %s\n"
-             % (newsub.actual_numline, e.__class__.__name__, e))
+    except (BadFormatError, MismatchTimeError, IndexNumError), e:
+        sys.stderr.write("%s: [at line %d] %s\n"
+             % (e.__class__.__name__, newsub.actual_numline, str(e)))
         in_file.close()
         out_file.close()
         tmpfile.write_back()
@@ -401,7 +510,8 @@ if __name__ == '__main__':
         in_file.close()
         out_file.close()
         tmpfile.write_back()
-        ue_msg = "\nUnknow error! maybe a bug. Shit!\nException is"
-        sys.stderr.write("%s: %s\n\n" % (ue_msg, repr(e)))
+        ue_msg = "\nUnknow error! surely a bug. Shit!\nException is"
+        sys.stderr.write("%s: [at line %d] %s\n\n"
+                         % (ue_msg, newsub.actual_numline, str(e)))
     in_file.close()
     out_file.close()
